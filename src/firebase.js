0ad24be80b1a4aeca523a884b6f3378b5da201b9
Sajal <sajal.kumar2003@gmail.com>
2024-10-24 23:32:18 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { firestore, auth };
